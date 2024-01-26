c30bad51b2b9245f9be053e76fda8c11faabfd9d
Орлов Евгений Павлович <orlov_ep@atb.su>
2024-01-26 18:08:49 +1000

    #24 migration naming fix

--- src/main/resources/db/migration/v10_create_table_base_skills.sql
+++ src/main/resources/db/migration/v10_create_table_base_skills.sql
@@ -1,6 +1,7 @@
 create table if not exists base_skills
 (
-    id          int auto_increment primary key,
-    name        tinytext not null,
-    description text
+    id                   int auto_increment primary key,
+    name                 tinytext not null,
+    description          text,
+    parent_base_skill_id int references base_skills
 );
